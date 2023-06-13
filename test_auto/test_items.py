f1b44d43d020fc6cc29d5bf4151ecc989af9dfed
Shaitan PC <isia.01@mail.ru>
2023-06-13 15:34:31 +0300

    auto test pytest

---
+++ test_auto/test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+import time
+link='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(30)
+    browser.implicitly_wait(5)
+    assert browser.find_element(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]'), 'Кнопка не найдена'
